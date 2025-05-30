9a017ce9eaea5a6cb26ce6303b1d5b2033cc1a0d
Toma955 <78325663+Toma955@users.noreply.github.com>
2025-05-30 17:15:55 +0200

    Dodana funkcija handleUpdateUser u UserController

--- server/controllers/UserController.js
+++ server/controllers/UserController.js
@@ -1,9 +1,11 @@
 const handleCreateUser = require("./user/createUser");
 const handleDeleteUser = require("./user/deleteUser");
 const handleGetUser = require("./user/getUser");
+const handleUpdateUser = require("./user/updateUser");
 
 module.exports = {
   handleCreateUser,
   handleDeleteUser,
-  handleGetUser
+  handleGetUser,
+  handleUpdateUser
 };
